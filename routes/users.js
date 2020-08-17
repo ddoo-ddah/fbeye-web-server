cb9a0e5c6289015db637939d010a2d7cf3d2f48c
Seokjin Yoon <sjy5386@gmail.com>
2020-08-18 00:00:47 +0900

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require('../lib/db');
+const crypto = require('../lib/crypto');
 const router = express.Router();
 
 /* GET users listing. */
@@ -7,7 +9,28 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/signin', (req, res, next) => {
-  res.render('users/signin');
+  if (!req.session.email) {
+    res.render('users/signin');
+  } else {
+    res.send('signed in.');
+  }
+});
+
+router.post('/signin', async (req, res, next) => {
+  const email = req.body["email"];
+  const password = await crypto.scrypt(req.body["password"], 'my salt', 32);
+  if (email && password) {
+    const client = await db.getClient();
+    const doc = client.db().collection('admin').findOne({
+      email
+    });
+    await client.close();
+    const result = doc && (doc.password === password);
+    if (result) {
+      req.session.email = email;
+    }
+    res.send(result);
+  }
 });
 
 module.exports = router;
