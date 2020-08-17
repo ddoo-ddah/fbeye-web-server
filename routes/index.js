2bb7500345ca0388ff77479516faf0a4652c433f
Seokjin Yoon <sjy5386@gmail.com>
2020-08-18 00:59:28 +0900

    Update index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  if (req.session.email) {
+    res.render('index', {
+      title: 'FBEye',
+      email: req.session.email
+    });
+  } else {
+    res.redirect('/users/signin');
+  }
 });
 
 module.exports = router;
