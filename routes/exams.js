442a2dd7965952f7366fdd1d86b389bb47077ad9
Seokjin Yoon <sjy5386@gmail.com>
2020-08-18 14:13:43 +0900

    Add view exams/new

--- routes/exams.js
+++ routes/exams.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const crypto = require('../lib/crypto');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     if (req.session.email) {
         res.send('exams');
+    } else {
+        res.redirect('/');
+    }
+});
+
+router.get('/new', async (req, res, next) => {
+    if (req.session.email) {
+        const accessCode = (await crypto.randomBytes(6)).toString('base64');
+        res.render('exams/new', {
+            admin: req.session.email,
+            accessCode
+        });
     } else {
         res.redirect('/');
     }
