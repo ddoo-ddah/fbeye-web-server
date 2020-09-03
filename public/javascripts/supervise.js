0ad5053958c2a6639695a2385ca4c52a84f71be3
pllapallpal <pllapallpal@gmail.com>
2020-09-03 20:25:10 +0900

    Make user status displayed in user list

--- public/javascripts/supervise.js
+++ public/javascripts/supervise.js
@@ -1,3 +1,5 @@
+// const db = require('../../lib/db'); 이거 안됨 TODO: 클라이언트 측에서 서버 DB에 연결하는 법
+
 const base64toBlob = (base64Data, contentType) => {
     contentType = contentType || '';
     let sliceSize = 512;
@@ -20,13 +22,23 @@ const base64toBlob = (base64Data, contentType) => {
 }
 
 // socket.io 관련 event
-window.onload = event => {
+window.onload = async event => {
 
-    const socket = io();
+    const userlist = document.querySelector('#user-list').children;
+    console.log(userlist);
+    // const client = await db.getClient();
+    // const users = await client.db().collection('users').find({ DB 사용 안됨
+    //     _id: { $in: exam.users }
+    // }).toArray();
+    for (let i = 0; i < userlist.length; ++i) { // todo: welcome했는지에 따라서 색깔 다르게 표시되도록 변경
+        if (i % 2) {
+            userlist[i].firstChild.className = 'list-group-item list-group-item-danger';
+        }
+    }
+
 
     /* 채팅 - welcome, chat, disconnect */
     const chatform = document.querySelector('#chat');
-    const chatlog = document.querySelector('#card-chatlog');
     const message = document.querySelector('input[name="message"]');
     chatform.addEventListener('submit', (event) => { // 메시지 전송
         event.preventDefault();
@@ -36,9 +48,12 @@ window.onload = event => {
         socket.emit('chat', message.value);
         message.value = '';
     });
-    socket.on('welcome', (data) => {
-        socket.broadcast.emit('welcome', '');
-    }).on('chat', (data) => {
+    const chatlog = document.querySelector('#card-chatlog');
+    const socket = io();
+    socket.emit('welcome', { // 입장할 때 이름 주기
+        
+    });
+    socket.on('chat', (data) => {
         chatlog.appendChild(createSpeechBubble(data.sender, data.message, data.timestamp));
         chatlog.scrollTop = chatlog.scrollHeight;
     }).on('disconnect', (data) => {
